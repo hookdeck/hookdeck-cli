3b2fdc75883cc385ce2db1626532b737a8c4f737
Phil Leggetter <phil@leggetter.co.uk>
2025-06-13 20:54:05 +0100

    feat: support passing project name to "project use {name}"

--- pkg/cmd/project_use.go
+++ pkg/cmd/project_use.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
 
@@ -41,41 +44,69 @@ func (lc *projectUseCmd) runProjectUseCmd(cmd *cobra.Command, args []string) err
 		return err
 	}
 
-	var currentProjectName string
-	projectNames := make([]string, len(projects))
-	for index, project := range projects {
-		projectNames[index] = project.Name
-		if project.Id == Config.Profile.ProjectId {
-			currentProjectName = project.Name
-		}
-	}
+	var selectedProject hookdeck.Project
+	var projectFound bool
 
-	var qs = []*survey.Question{
-		{
-			Name: "project_name",
-			Prompt: &survey.Select{
-				Message: "Select Project",
-				Options: projectNames,
-				Default: currentProjectName,
+	if len(args) > 0 {
+		argProjectName := args[0]
+		for _, p := range projects {
+			if strings.EqualFold(p.Name, argProjectName) {
+				selectedProject = p
+				projectFound = true
+				break
+			}
+		}
+
+		if !projectFound {
+			return fmt.Errorf("error: project '%s' not found", argProjectName)
+		}
+	} else {
+		var currentProjectName string
+		projectNames := make([]string, len(projects))
+		for index, project := range projects {
+			projectNames[index] = project.Name
+			if project.Id == Config.Profile.ProjectId {
+				currentProjectName = project.Name
+			}
+		}
+
+		var qs = []*survey.Question{
+			{
+				Name: "project_name",
+				Prompt: &survey.Select{
+					Message: "Select Project",
+					Options: projectNames,
+					Default: currentProjectName,
+				},
+				Validate: survey.Required,
 			},
-			Validate: survey.Required,
-		},
-	}
-
-	answers := struct {
-		ProjectName string `survey:"project_name"`
-	}{}
-
-	if err = survey.Ask(qs, &answers); err != nil {
-		return err
-	}
-
-	var project hookdeck.Project
-	for _, tempProject := range projects {
-		if answers.ProjectName == tempProject.Name {
-			project = tempProject
 		}
+
+		answers := struct {
+			ProjectName string `survey:"project_name"`
+		}{}
+
+		if err = survey.Ask(qs, &answers); err != nil {
+			return err
+		}
+
+		for _, tempProject := range projects {
+			if answers.ProjectName == tempProject.Name {
+				selectedProject = tempProject
+				projectFound = true // Set projectFound to true as project is selected via survey
+				break
+			}
+		}
+		// In interactive mode, if a project is selected, it's implicitly found.
+		// If survey.Ask succeeds, a project name is selected.
+		// The loop above will find it. If somehow it doesn't (e.g. projects list changed),
+		// selectedProject would be zero-valued, leading to issues in UseProject.
+		// However, the current logic assumes a match will be found if survey succeeds.
 	}
 
-	return Config.UseProject(project.Id, project.Mode)
+	if !projectFound && len(args) == 0 { // This case should ideally not be hit if survey logic is correct
+		return fmt.Errorf("error: no project selected interactively")
+	}
+
+	return Config.UseProject(selectedProject.Id, selectedProject.Mode)
 }
